8c37167d0416d5d02b27673235edd98fcb9dcbe0
Mahmoud Shtayeh <mshtayeh.c@stc.com.sa>
2025-06-19 14:01:43 +0300

    Structure Kafka listener

---
+++ src/main/java/com/amigoscode/kafka/KafkaListeners.java
@@ -0,0 +1,14 @@
+package com.amigoscode.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class KafkaListeners {
+    @KafkaListener(topics = "amigoscode", groupId = "amigosccodeGroup")
+    public void amigoscodeListener(String messagePayload) {
+        log.info("[Received] message: {}", messagePayload);
+    }
+}
